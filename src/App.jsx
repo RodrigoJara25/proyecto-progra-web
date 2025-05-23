bf3981c7063987aae8966e7503d5e2419c6169ca
AlonsoAV-dev <alonsoalmerco.v@gmail.com>
2025-05-23 01:07:40 -0500

    Lista de Productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import ListadoProducto from './pages/ListadoProducto/ListadoProducto'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header/>
+      <ListadoProducto/>
     </>
   )
 }
---
+++ src/pages/ListadoProducto/ListadoProducto.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TablaProductos from '../../components/TablaProductos/TablaProductos';
+
+const ListadoProducto = () => {
+    return (
+        <>
+            <TablaProductos />
+        </>
+    );
+}
+
+export default ListadoProducto;
