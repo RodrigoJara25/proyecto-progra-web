d748e50e6c8e6fc667a50874166cc8dbbe86c360
Andrew <20221260@aloe.ulima.edu.pe>
2025-05-24 10:27:26 -0500

    commit login con css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,14 +6,24 @@ function Login() {
         <div className="login">
             <div className="login-card">
                 <h2>Iniciar sesion</h2>
+                
                 <div className="correo">
                     <p>Correo</p>
-                    <input type="text" placeholder="usuario@gmail.com2"/>
+                    <input type="text" placeholder="usuario@gmail.com"/>
                 </div>
+               
                 <div className="password">
                     <p>Contraseña</p>
                     <input type="password" placeholder="Contraseña"/>
                 </div>
+                
+                <button className="bot Iniciar Sesion">Iniciar sesión</button>
+                
+                <div className="enlaces">
+                        <p className="registro">Registrarme</p>
+                        <p className="olvide-contraseña">Olvide mi contraseña</p>
+                </div>
+           
             </div>
         </div>
     );
